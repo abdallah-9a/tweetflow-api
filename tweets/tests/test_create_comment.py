45313d600fe6de1e02b2946c584c4e7518825fa0
abdallah-rabie <abdallah.rabie.fayez@gmail.com>
2025-10-05 09:46:09 +0300

    test: tests for create comment and list comments endpoints

---
+++ tweets/tests/test_create_comment.py
@@ -0,0 +1,41 @@
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from tweets.models import Tweet, Comment
+from accounts.models import User
+
+
+class TestCreateComment(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="user", email="user@gmail.com", password="user1234"
+        )
+        self.tweet = Tweet.objects.create(user=self.user, content="Tweet")
+        self.url = reverse("comment", kwargs={"pk": self.tweet.pk})
+        self.data = {"content": "Comment"}
+
+    def authenticate(self):
+        self.client.force_authenticate(user=self.user)
+
+    def test_create_comment_success(self):
+        self.authenticate()
+        response = self.client.post(self.url, {"content": "comment"})
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(
+            Comment.objects.filter(user=self.user, tweet=self.tweet).exists()
+        )
+
+    def test_create_comment_without_data(self):
+        self.authenticate()
+        response = self.client.post(self.url, {})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_comment_unauthenticated(self):
+        response = self.client.post(self.url, self.data)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_create_comment_with_not_found_tweet(self):
+        self.authenticate()
+        url = reverse("comment", kwargs={"pk": 10})
+        response = self.client.post(url, self.data)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
---
+++ tweets/tests/test_list_comment.py
@@ -0,0 +1,42 @@
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from tweets.models import Tweet, Comment
+from accounts.models import User
+
+
+class TestListComment(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="user", email="user@gmail.com", password="user1234"
+        )
+        self.tweet = Tweet.objects.create(user=self.user, content="Tweet")
+        self.url = reverse("comments", kwargs={"pk": self.tweet.pk})
+
+    def authenticate(self):
+        self.client.force_authenticate(user=self.user)
+
+    def test_list_comments_success(self):
+        self.authenticate()
+        Comment.objects.create(user=self.user, tweet=self.tweet, content="First")
+        Comment.objects.create(user=self.user, tweet=self.tweet, content="Second")
+
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+    def test_list_comments_empty(self):
+        self.authenticate()
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 0)
+
+    def test_list_comments_unauthenticated(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_list_comments_missing_tweet(self):
+        self.authenticate()
+        missing_url = reverse("comments", kwargs={"pk": 999999})
+        response = self.client.get(missing_url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
