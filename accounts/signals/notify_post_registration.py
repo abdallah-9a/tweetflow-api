6ead0f0fec2758ee2e948b46e165bbbe77828c84
abdallah-rabie <abdallah.rabie.fayez@gmail.com>
2026-04-05 17:19:35 +0200

    refactor(accounts): convert post-registration notification to async Celery task

--- accounts/signals/notify_post_registration.py
+++ accounts/signals/notify_post_registration.py
@@ -1,10 +1,11 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+from django.db import transaction
 from accounts.models import Profile
-from interactions.utils import create_notification
+from accounts.tasks import send_welcome_notification_task
 
 
 @receiver(post_save, sender=Profile)
 def on_registration(sender, instance, created, **kwargs):
     if created:
-        create_notification(receiver=instance.user, verb="welcome")
+        transaction.on_commit(lambda: send_welcome_notification_task.delay(instance.user.id))
--- accounts/tasks.py
+++ accounts/tasks.py
@@ -1,6 +1,19 @@
 from celery import shared_task
+from django.contrib.auth import get_user_model
 from .utils import Util
+from interactions.utils import create_notification
+
+User = get_user_model()
 
 @shared_task
 def send_email_task(data):
     Util.send_email(data)
+
+
+@shared_task
+def send_welcome_notification_task(user_id):
+    try:
+        user = User.objects.get(id=user_id)
+        create_notification(receiver=user, verb="welcome")
+    except User.DoesNotExist:
+        pass
